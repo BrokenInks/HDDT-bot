3a7f3575357dfcc94055dc7fcc0b2e2b7fc3f53d
Smokyo <69753561+Smokyo@users.noreply.github.com>
2021-02-25 18:23:33 +0300

    test

---
+++ commands/sumd.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'sum/',
+    execute(message, args, client) {
+        const numArgs = args.map(x => parseFloat(x));
+    const sum = numArgs.reduce((counter, x) => counter /= x);
+    message.reply(`Сумма этих чисел будет ${sum}!`);
+    },
+};
---
+++ main.js
@@ -0,0 +1,101 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const { RichEmbed } = require("discord.js");
+const beautify = require("beautify");
+const { prefix, token } = require('./config.json');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+const cooldowns = new Discord.Collection();
+
+client.once('ready', () => {
+	console.log('Хикару запустилась!');
+	setInterval(function(){
+		let stausi = [
+	  'Как верефицироваться',
+	  'на котиков',
+	  'на свой код',
+	  'на TC Team'
+		]
+		let aye_status = stausi[Math.floor(Math.random() * stausi.length)]
+		
+		client.user.setActivity(`${aye_status}`,{ type: 'WATCHING'})
+		},60000);
+	  });
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+    
+	const command = client.commands.get(commandName)
+		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+	if (!command) return;
+
+	if (command.guildOnly && message.channel.type !== 'text') {
+		return message.reply('Я не могу выполнить эту команду в личных сообщениях!');
+	}
+
+	if (command.args && !args.length) {
+			let reply = `Ты не правильно написал команду, ${message.author}!`;
+		
+				if (command.usage) {
+					reply += `\nПравильное написание команды ${command.name}: \`${prefix}${command.name} ${command.usage}\``;
+				}
+		
+				return message.channel.send(reply);
+			}
+
+
+			if (!cooldowns.has(command.name)) {
+				cooldowns.set(command.name, new Discord.Collection());
+			}
+			
+			const now = Date.now();
+			const timestamps = cooldowns.get(command.name);
+			const cooldownAmount = (command.cooldown || 3) * 1000;
+			
+			if (timestamps.has(message.author.id)) {
+				if (!cooldowns.has(command.name)) {
+					cooldowns.set(command.name, new Discord.Collection());
+				}
+				
+				const now = Date.now();
+				const timestamps = cooldowns.get(command.name);
+				const cooldownAmount = (command.cooldown || 3) * 1000;
+				
+				if (timestamps.has(message.author.id)) {
+					if (timestamps.has(message.author.id)) {
+						const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+					
+						if (now < expirationTime) {
+							const timeLeft = (expirationTime - now) / 1000;
+							return message.reply(`пожалуйста подождите ${timeLeft.toFixed(1)} секунд что бы использовать команду \`${command.name}\` `);
+						}
+
+						timestamps.set(message.author.id, now);
+setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+					}
+				}
+			}
+
+try {
+	command.execute(client, message, args);
+} catch (error) {
+	console.error(error);
+	message.reply('Ошибка!');
+}
+});
+
+client.login(token);
